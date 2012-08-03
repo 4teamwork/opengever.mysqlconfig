04f1b4a9babbe5149de258edd833593f896258b0
philippe.gross <philippe.gross@4teamwork.ch>
2012-08-03 09:34:00 +0200

    Added important configuration options in the mysql db conf and creation script.

--- opengever/mysqlconfig/create_database.sql
+++ opengever/mysqlconfig/create_database.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE opengever;
-GRANT ALL ON opengever.* TO 'opengever'@'localhost' identified by 'opengever';
+CREATE DATABASE opengever CHARACTER SET utf8;
+GRANT ALL ON opengeverphvs.* TO 'opengever'@'localhost' identified by 'opengever';
 FLUSH PRIVILEGES;
--- opengever/mysqlconfig/dev_create_database.sql
+++ opengever/mysqlconfig/dev_create_database.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE opengeverdev;
+CREATE DATABASE opengeverdev CHARACTER SET utf8;
 GRANT ALL ON opengeverdev.* TO 'opengeverdev'@'localhost' identified by 'opengeverdev';
 FLUSH PRIVILEGES;
