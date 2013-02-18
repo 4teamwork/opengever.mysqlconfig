eb2072bb869e88c78b98c0ac2159545d0ff4fc01
David Erni <david.erni@4teamwork.ch>
2013-02-18 18:13:22 +0100

    corrected initial database name

--- opengever/mysqlconfig/create_database.sql
+++ opengever/mysqlconfig/create_database.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE opengever CHARACTER SET utf8;
-GRANT ALL ON opengeverphvs.* TO 'opengever'@'localhost' identified by 'opengever';
+GRANT ALL ON opengever.* TO 'opengever'@'localhost' identified by 'opengever';
 FLUSH PRIVILEGES;
